18f422b9c6d9e31eb24522f23005ff32544b2bd9
raniac <leibingye@outlook.com>
2019-09-23 10:17:47 +0800

    Added start.sh as entry.

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ ADD . /neuro-learn/
 
 RUN cp -f /neuro-learn/nginx.conf /etc/nginx
 
-CMD ["/bin/bash"]
+CMD ["sh","/neuro-learn/start.sh"]
---
+++ start.sh
@@ -0,0 +1,6 @@
+service nginx start;
+service rabbitmq-server start;
+cd /neuro-learn/app/;
+uwsgi -d --ini uwsgi.ini;
+export C_FORCE_ROOT='True';
+python3 manage.py celeryd -l info;
