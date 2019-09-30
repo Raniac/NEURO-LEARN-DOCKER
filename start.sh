7101a65066035071edb2af1975ea2101720c47b6
raniac <leibingye@outlook.com>
2019-09-30 10:33:10 +0800

    Changed uwsgi logging.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 service nginx start;
 service rabbitmq-server start;
 cd /neuro-learn/app/;
-uwsgi -d --ini uwsgi.ini;
+nohup uwsgi --ini uwsgi.ini >> uwsgi.log &;
 export C_FORCE_ROOT='True';
 python3.6 manage.py celeryd -l info;
