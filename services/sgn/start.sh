2f3c536c3d5dd07d8d752593bfee067d0f0e1377
Raniac <leibingye@outlook.com>
2020-07-05 12:23:27 +0800

    Added utils for hosts configuration.

--- services/sgn/start.sh
+++ services/sgn/start.sh
@@ -2,4 +2,4 @@ service nginx start;
 service redis-server start;
 cd /nld_sgn/app;
 nohup celery worker -A main.celery --loglevel=info >> celery.log &
-gunicorn main:app --bind 0.0.0.0:8000 --workers 4 --log-level debug;
+gunicorn main:app --bind 0.0.0.0:7014 --workers 4 --log-level debug;
---
+++ utils/hosts-config.sh
@@ -0,0 +1,3 @@
+echo "127.0.0.1:1470 commons.neurolearn.com" >> /etc/hosts
+echo "127.0.0.1:4701 ml.neurolearn.com" >> /etc/hosts
+echo "127.0.0.1:7014 sgn.neurolearn.com" >> /etc/hosts
