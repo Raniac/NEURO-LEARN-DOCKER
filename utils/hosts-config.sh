21f4d8b8df79fa9e717ab4fa4df03ee50064e346
Raniac <leibingye@outlook.com>
2020-07-24 22:46:18 +0800

    Minor changes.

--- utils/hosts-config.sh
+++ utils/hosts-config.sh
@@ -2,4 +2,4 @@
 120.79.49.129   neurolearn.com         # port 80
 120.79.49.129   commons.neurolearn.com # port 1470
 120.79.49.129   ml.neurolearn.com      # port 4701
-120.79.49.129   sgn.neurolearn.com     # port 4701
+120.79.49.129   sgn.neurolearn.com     # port 7014
