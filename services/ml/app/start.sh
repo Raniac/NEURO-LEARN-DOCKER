391fcb471a6038d42b3b61bd5a3bdb3cedae145b
Raniac <leibingye@outlook.com>
2020-07-29 21:23:24 +0800

    Added log.

--- services/ml/app/start.sh
+++ services/ml/app/start.sh
@@ -1,4 +1,4 @@
 cd /nls-ml/app;
 export C_FORCE_ROOT=true;
-nohup celery worker -A main.celery --loglevel=info >> celery.log &
-gunicorn main:app --bind 0.0.0.0:4701 --workers 4 --log-level debug;
+nohup celery worker -A main.celery --loglevel=info >> /nls-ml/log/celery.log &
+nohup gunicorn main:app --bind 0.0.0.0:4701 --workers 4 --log-level debug >> /nls-ml/log/gunicorn.log &
--- services/sgn/start.sh
+++ services/sgn/start.sh
@@ -1,4 +1,4 @@
 cd /nls-sgn/app;
 export C_FORCE_ROOT=true;
-nohup celery worker -A main.celery --loglevel=info >> celery.log &
-gunicorn main:app --bind 0.0.0.0:7014 --workers 4 --log-level debug;
+nohup celery worker -A main.celery --loglevel=info >> /nls-sgn/log/celery.log &
+nohup gunicorn main:app --bind 0.0.0.0:7014 --workers 4 --log-level debug >> /nls-sgn/log/gunicorn.log &
