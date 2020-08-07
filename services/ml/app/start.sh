9c6bcdd661d624adb136e085a7b6e68efac56ce2
raniac <leibingye@outlook.com>
2020-08-07 15:06:25 +0800

    Fix: celery initiation failure.

--- services/ml/app/start.sh
+++ services/ml/app/start.sh
@@ -1,4 +1,4 @@
 cd /nls-ml/app;
 export C_FORCE_ROOT=true;
 nohup celery worker -A main.celery --loglevel=info >> /nls-ml/log/celery.log &
-gunicorn main:app --bind 0.0.0.0:4701 --workers 4 --log-level debug --access-logfile /nls-ml/log/gunicorn.access.log --error-logfile /nls-ml/log/gunicorn.error.log
+gunicorn main:app --bind 0.0.0.0:4701 --workers 4 --log-level debug --access-logfile /nls-ml/log/gunicorn.access.log --error-logfile /nls-ml/log/gunicorn.error.log;
--- services/sgn/start.sh
+++ services/sgn/start.sh
@@ -1,4 +1,4 @@
 cd /nls-sgn/app;
 export C_FORCE_ROOT=true;
 nohup celery worker -A main.celery --loglevel=info >> /nls-sgn/log/celery.log &
-gunicorn main:app --bind 0.0.0.0:7014 --workers 4 --log-level debug --access-logfile /nls-sgn/log/gunicorn.access.log --error-logfile /nls-sgn/log/gunicorn.error.log
+gunicorn main:app --bind 0.0.0.0:7014 --workers 4 --log-level debug --access-logfile /nls-sgn/log/gunicorn.access.log --error-logfile /nls-sgn/log/gunicorn.error.log;
