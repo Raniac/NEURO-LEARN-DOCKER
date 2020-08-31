e7de987f4f7f5db1c79ecc3700b4c487fc17167e
raniac <leibingye@outlook.com>
2020-08-31 13:20:33 +0800

    Fix: docker container fail to stay running.

--- services/sgn/start.sh
+++ services/sgn/start.sh
@@ -1,4 +1,4 @@
 cd /nls-sgn/app;
 export C_FORCE_ROOT=true;
 gunicorn main:app -D --bind 0.0.0.0:7014 --workers 4 --log-level debug --access-logfile /nls-sgn/log/gunicorn.access.log --error-logfile /nls-sgn/log/gunicorn.error.log;
-nohup celery worker -A main.celery --loglevel=info >> /nls-sgn/log/celery.log &
+nohup celery worker -A main.celery --loglevel=info >> /nls-sgn/log/celery.log
