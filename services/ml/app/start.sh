b5a824c3554dd25846463cc05a47f052ef85bc67
raniac <leibingye@outlook.com>
2020-08-31 14:13:21 +0800

    Fix: cannot redirect celery output as log.

--- services/ml/app/start.sh
+++ services/ml/app/start.sh
@@ -1,4 +1,4 @@
 cd /nls-ml/app;
 export C_FORCE_ROOT=true;
 gunicorn main:app -D --bind 0.0.0.0:4701 --workers 4 --log-level debug --access-logfile /nls-ml/log/gunicorn.access.log --error-logfile /nls-ml/log/gunicorn.error.log;
-nohup celery worker -A main.celery --loglevel=info >> /nls-ml/log/celery.log
+celery worker -A main.celery -l info -f /nls-ml/log/celery.log
--- services/sgn/start.sh
+++ services/sgn/start.sh
@@ -1,4 +1,4 @@
 cd /nls-sgn/app;
 export C_FORCE_ROOT=true;
 gunicorn main:app -D --bind 0.0.0.0:7014 --workers 4 --log-level debug --access-logfile /nls-sgn/log/gunicorn.access.log --error-logfile /nls-sgn/log/gunicorn.error.log;
-nohup celery worker -A main.celery --loglevel=info >> /nls-sgn/log/celery.log
+celery worker -A main.celery -l info -f /nls-sgn/log/celery.log
