e50638dc48be7bd9470654f5a5cc28a509817ed7
raniac <leibingye@outlook.com>
2020-09-14 10:14:55 +0800

    Minor changes.

--- utils/HostsConfig.sh
+++ utils/HostsConfig.sh
@@ -1,6 +1,6 @@
 # CHANGE the corresponding host IP's
 120.79.49.129   neurolearn.com         # port 80
-120.79.49.129   api.neurolearn.com # port 1470
+120.79.49.129   api.neurolearn.com     # port 1470
 120.79.49.129   ml.neurolearn.com      # port 4701
 120.79.49.129   sgn.neurolearn.com     # port 7014
 120.79.49.129   db.neurolearn.com      # port 3306
