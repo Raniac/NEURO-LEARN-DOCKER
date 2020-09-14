1ab1e689c83bee740571efcf8c2036d942176122
raniac <leibingye@outlook.com>
2020-09-14 10:11:30 +0800

    Output fetched result to file.

--- utils/ResultFetcher.py
+++ utils/ResultFetcher.py
@@ -1,5 +1,6 @@
 import pymysql
 import json
+import sys
 import traceback
 
 class MYSQLDB():
@@ -48,8 +49,24 @@ def get_results_by_taskids(mysql, task_ids):
     except:
         traceback.print_exc()
 
+class Logger(object):
+    def __init__(self, fileN='Default.log'):
+        self.terminal = sys.stdout
+        self.log = open(fileN, 'a')
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
 if __name__ == '__main__':
     mysql = MYSQLDB(host="db.neurolearn.com", user="neurolearn", pwd="nl4444_", db="neurolearn")
+
+    sys.stdout = Logger('results.txt') # output console log
+    
+    # Obtain task ids to show
     task_ids = []
     with open('cache.txt', 'r') as t:
         for i in t.readlines():
