535ca52531467552d17679ab742e1f174cf6965c
raniac <leibingye@outlook.com>
2020-10-12 14:08:01 +0800

    Added HdfsCluster.

---
+++ utils/HdfsCluster/README.md
@@ -0,0 +1,5 @@
+# HDFS Cluster
+
+## Build HDFS Cluster
+
+- [Hadoop分布式集群搭建](https://www.cnblogs.com/skyturtle/p/9979413.html)
--- utils/dm/app/config/settings.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from hdfs.client import InsecureClient
-
-app = FastAPI()
-
-client = InsecureClient("http://hdfs.neurolearn.com:50070", user="hadoop")
-
-BASE_URL = "/rest/dmservice"
