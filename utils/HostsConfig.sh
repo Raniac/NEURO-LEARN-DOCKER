1d8492e927dbb1a2b1861d7b2ddf9dd4f63d87bf
raniac <leibingye@outlook.com>
2020-10-16 11:20:51 +0800

    Added hosts config for sa service.

--- utils/HostsConfig.sh
+++ utils/HostsConfig.sh
@@ -2,5 +2,6 @@
 120.79.49.129   neurolearn.com         # port 80
 120.79.49.129   api.neurolearn.com     # port 1470
 120.79.49.129   ml.neurolearn.com      # port 4701
+120.79.49.129   sa.neurolearn.com      # port 7410
 120.79.49.129   sgn.neurolearn.com     # port 7014
 120.79.49.129   db.neurolearn.com      # port 3306
